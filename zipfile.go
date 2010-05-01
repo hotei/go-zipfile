0cff57e7ea2520b9e5acdc8587499b8c65a3eebd
David Rook <ravenstone13@cox.net>
2010-05-01 00:48:03 -0400

    added crc32 verify (in ListFile)
    	modified:   zipfile.go

--- zipfile.go
+++ zipfile.go
@@ -11,15 +11,18 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"hash/crc32"
 	"io"
+	//	"io/ioutil"
 	"os"
 	"compress/flate"
 	//    "unsafe"
 )
 
-const DEBUG = true
+const DEBUG = false
+
 const (
-	DOSEPOCH        = 1980
+	MSDOS_EPOCH     = 1980
 	ZIP_LocalHdrSig = "PK\003\004"
 	ZIP_CentDirSig  = "PK\001\002"
 	ZIP_STORED      = 0
@@ -28,10 +31,10 @@ const (
 
 type ZipFile struct {
 	fileName string
-	fpin     *os.File
+	//	fpin     *os.File   // does it make sense to carry this in struct?
 	fileSize int64
+	numFiles uint32
 	headers  []ZipLocalHeader
-	readposn int64
 }
 
 type ZipLocalHeader struct {
@@ -51,37 +54,40 @@ type ZipLocalHeader struct {
 	dataOffset   int64 // where we seek to pick up the data
 }
 
-// test function to read thru file headers
+
+// function to read thru whole file picking up all file headers into headers[]
+// keep fileName, calculate archive size fileSize, count numFiles
 func (z *ZipFile) Init(filename string) bool {
 	var n int
 	var curPos int64
 	var newPos int64
 	var err os.Error
+	var fpin *os.File
 
 	z.fileName = filename
 	z.headers = make([]ZipLocalHeader, 0, 100)
-	z.fpin, err = os.Open(filename, os.O_RDONLY, 0666)
+	fpin, err = os.Open(filename, os.O_RDONLY, 0666)
 	// err could be file not found, file found but not accessible etc
 	// in any case test fails
 	if err != nil {
 		return false
 	}
-	defer z.fpin.Close()
-	z.fileSize, err = z.fpin.Seek(0, 2) // get file size
+	defer fpin.Close()
+	z.fileSize, err = fpin.Seek(0, 2) // get file size
 	if DEBUG {
-		fmt.Printf("file size = %d\n", z.fileSize)
+		fmt.Printf("%s size = %d\n", z.fileName, z.fileSize)
 	}
 	if err != nil {
 		return false
 	}
-	_, err = z.fpin.Seek(0, 0) // back to beginning
+	_, err = fpin.Seek(0, 0) // back to beginning
 
 	hdr := new(ZipLocalHeader)
 	//    fmt.Printf("size of hdr = %d\n", unsafe.Sizeof(*hdr))
 	var hdrData [30]byte // size of header data fixed fields only
 
 	for {
-		n, err = z.fpin.Read(&hdrData)
+		n, err = fpin.Read(&hdrData)
 		if err != nil || n != 30 { /* TODO problem */
 			fmt.Printf("Header read failed\n")
 			os.Exit(1)
@@ -139,20 +145,22 @@ func (z *ZipFile) Init(filename string) bool {
 		//        fmt.Printf("extraFldLen = %d\n", hdr.extraFldLen)
 
 		hdr.fileName = make([]byte, hdr.fileNameLen, hdr.fileNameLen)
-		n, err = z.fpin.Read(hdr.fileName)
+		n, err = fpin.Read(hdr.fileName)
 		if err != nil || n != int(hdr.fileNameLen) { /* TODO problem */
 		}
-		fmt.Printf("filename = %s\n", hdr.fileName)
+		if DEBUG {
+			fmt.Printf("zip includes file: %s\n", hdr.fileName)
+		}
 
 		extra := make([]byte, hdr.extraFldLen, hdr.extraFldLen)
-		n, err = z.fpin.Read(extra)
+		n, err = fpin.Read(extra)
 		if err != nil || n != int(hdr.extraFldLen) { /* TODO problem */
 		}
 		//        fmt.Printf("extra = %v\n", extra)
-		curPos, err = z.fpin.Seek(0, 1)
+		curPos, err = fpin.Seek(0, 1)
 		//        fmt.Printf("current position is %d\n", curPos )
 		hdr.dataOffset = curPos
-		newPos, err = z.fpin.Seek(int64(hdr.compressSize), 1) // advance to next header
+		newPos, err = fpin.Seek(int64(hdr.compressSize), 1) // advance to next header
 		if newPos != int64(hdr.compressSize)+curPos {
 			fmt.Printf("advance to next header failed %d != %d\n", newPos, int64(hdr.compressSize)+curPos)
 			os.Exit(1)
@@ -182,6 +190,7 @@ func (z *ZipFile) Init(filename string) bool {
 			z.headers = z.headers[0 : l+1]
 			z.headers[l] = *hdr
 		}
+		z.numFiles++
 	}
 	if DEBUG {
 		fmt.Printf("Read in %d header record(s)\n", len(z.headers))
@@ -203,6 +212,7 @@ func (z *ZipFile) ListOfFiles() []string {
 	return fileList // success
 }
 
+
 // send specified zip'd data to stdout
 func (z *ZipFile) ListZip(which int) {
 	var n int
@@ -214,54 +224,72 @@ func (z *ZipFile) ListZip(which int) {
 	if z.headers[which].compressMeth == ZIP_DEFLATED {
 		compData := make([]byte, z.headers[which].compressSize)
 		n, err = inf.Read(compData)
-		fmt.Printf("Read in %d bytes of uncompressed data\n", n)
-		if err != nil {
+		if err != nil || uint32(n) != z.headers[which].compressSize {
 			fmt.Printf("zip data read failed\n")
 			os.Exit(1)
 		}
+		fmt.Printf("Read in %d bytes of compressed (deflated) data\n", n)
 
-		fmt.Printf("slice [0:80] of zip'd data: %v\n", compData[0:80])
 		// got it in RAM, now need to expand it
 		b := new(bytes.Buffer)          // create a new buffer with io methods
-		in := bytes.NewBuffer(compData) // copy compressed data into new buf
-		r := flate.NewInflater(in)
+		in := bytes.NewBuffer(compData) // fill new buffer with compressed data
+		r := flate.NewInflater(in)      // attach a reader to the buffer
 		if err != nil {
 			fmt.Printf("%s has err = %v\n", z.fileName, err)
 			os.Exit(1)
 		}
-		defer r.Close()
-		b.Reset()					// empty out the buffer
-		n2, err = io.Copy(b, r)		// now fill it from compressed data
+		defer r.Close()         // make sure we eventually close the reader
+		b.Reset()               // empty out the buffer
+		n2, err = io.Copy(b, r) // now fill buffer again from compressed data in the reader object
 		if err != nil {
 			fmt.Printf("%s has err = %v\n", z.fileName, err)
 			os.Exit(1)
-			// fmt.Printf("r = %v\n", r)
-			r.Close()
 		}
-		n2, err = io.Copy(os.Stdout, b)
-		
-		//s := b.String()				// 
+
+		expdData := make([]byte, z.headers[which].unComprSize)
+		n, err = b.Read(expdData)
+		if err != nil {
+			fmt.Printf("%s has err = %v\n", z.fileName, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Size of expdData = %d\n", len(expdData))
+		mycrc32 := crc32.ChecksumIEEE(expdData)
+		fmt.Printf("Computed Checksum = %0x, stored checksum = %0x\n", mycrc32, z.headers[which].crc32)
+
+		if mycrc32 != z.headers[which].crc32 {
+			fmt.Printf("CRC32 mismatch for %s\n", z.fileName)
+			os.Exit(1)
+		}
+		n2, err = io.Copy(os.Stdout, b) // copy the buffer to output
+		if n2 != int64(z.headers[which].unComprSize) {
+			fmt.Printf("%s has err = %v\n", z.fileName, os.EINVAL)
+			os.Exit(1)
+		}
+
+		if err != nil {
+			fmt.Printf("%s has err = %v\n", z.fileName, err)
+			os.Exit(1)
+		}
+		// if buffer data is text we can do this
+		//s := b.String()				//
 		//fmt.Printf("OUTPUT of inflater: \n%s\n", s)
-		n2 = n2
-		//expdData := make([]byte, z.headers[which].unComprSize)
-		//expdData = expdData
 	}
 
 	if z.headers[which].compressMeth == ZIP_STORED {
 		expdData := make([]byte, z.headers[which].unComprSize)
 		n, err = inf.Read(expdData)
-		fmt.Printf("Read in %d bytes of uncompressed data\n", n)
+		fmt.Printf("Read in %d bytes of uncompressed (stored) data\n", n)
 		if err != nil {
 			fmt.Printf("zip data read failed\n")
 			os.Exit(1)
 		}
-		fmt.Printf("%v\n", expdData)
+		fmt.Printf("%s\n", string(expdData))
 	}
 }
 
-func main() {
-	var ok bool = true
-	fmt.Printf("hello\n")
+// list files in named archive
+func test_1() {
 	flag.Parse()
 	fmt.Printf("Flag got %d args on cmd line after command name\n", flag.NArg())
 	if flag.NArg() == 0 { // do nothing
@@ -270,27 +298,66 @@ func main() {
 			fmt.Printf("%d %s\n", i, flag.Arg(i))
 
 			myzipper := new(ZipFile)
-			ok = myzipper.Init(flag.Arg(i)) // read headers into array
-			fmt.Printf("file[1] zip'd size = %d\n", myzipper.headers[0].compressSize)
-			fmt.Printf("file[1] unzip'd size = %d\n", myzipper.headers[0].unComprSize)
+			ok := myzipper.Init(flag.Arg(i)) // read headers into array, get size
+			if !ok { /* TODO */
+			}
 
 			var files []string
 			files = myzipper.ListOfFiles()
 			if len(files) == 0 {
-				fmt.Printf("No files in zipfile %s ...???\n", myzipper.fileName)
+				if DEBUG {
+					fmt.Printf("No files in zipfile %s ...???\n", myzipper.fileName)
+				}
+				continue
 			}
 			if files == nil || !ok { /* TODO problem */
 			} else {
 				for ndx, val := range files {
-					fmt.Printf("file[%d] = %s\n", ndx+1, val)
+					fmt.Printf("file[%d] = %s\n", ndx, val)
 				}
 			}
 		}
 	}
+}
 
+func test_2() {
+}
+
+
+func test_3(fname string) {
 	myzipper := new(ZipFile)
-	ok = myzipper.Init("stuf.zip") // read headers into array
-	myzipper.ListZip(0)
+	ok := myzipper.Init(fname) // read - this one is stored
+	if !ok {
+		fmt.Printf("Can't open requested file: %s\n", fname)
+		return
+	}
+	myzipper.ListZip(0) // list the first file to stdout
+}
+
+func test_4(fname string) {
+	myzipper := new(ZipFile)
+	ok := myzipper.Init(fname)
+	if !ok {
+		fmt.Printf("Can't open requested file: %s\n", fname)
+		return
+	}
+	if myzipper.numFiles >= 2 {
+		fmt.Printf("file[0] zip'd size = %d\n", myzipper.headers[0].compressSize)
+		fmt.Printf("file[0] unzip'd size = %d\n", myzipper.headers[0].unComprSize)
+		fmt.Printf("file[1] zip'd size = %d\n", myzipper.headers[1].compressSize)
+		fmt.Printf("file[1] unzip'd size = %d\n", myzipper.headers[1].unComprSize)
+	}
+}
+
+// M A I N ----------------------------------------------------------------------- M A I N
+func main() {
+	fmt.Printf("<starting test of zipfile>\n")
+	test_1() // list titles of all files
+	// test_2()
+	test_3("stuf.zip") // dump stuf.zip to stdout
+	test_3("mini.zip") // dump mini.zip to stdout
+	test_4("phpBB-2.0.20.zip")
+	fmt.Printf("<end of test of zipfile>\n")
 	os.Exit(0)
 }
 
@@ -298,7 +365,7 @@ func zip_decode_date(d uint16) {
 	var year, month, day uint16
 	year = d & 0xfe00
 	year >>= 9
-	//fmt.Printf("year = %d\n", year+DOSEPOCH)
+	//fmt.Printf("year = %d\n", year + MSDOS_EPOCH)
 
 	month = d & 0x01e0
 	month >>= 5
