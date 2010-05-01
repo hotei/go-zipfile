ef29d311d8ba34bce286ca9746f95102191d8ae2
David Rook <ravenstone13@cox.net>
2010-05-01 01:01:37 -0400

    LGTM
    	modified:   zipfile.go

--- zipfile.go
+++ zipfile.go
@@ -4,6 +4,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// <David Rook> ravenstone13@cox.net
+// This is a work-in-progress
 
 package main
 
